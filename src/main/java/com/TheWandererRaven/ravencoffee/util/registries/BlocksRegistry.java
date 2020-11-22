58d2094b788f7bc6f257318ef8ee8a262ec61481
TheWandererRaven <gabrielcorderor@gmail.com>
2020-11-21 23:36:00 -0600

    Added Bush block rendering

--- src/main/java/com/TheWandererRaven/ravencoffee/util/registries/BlocksRegistry.java
+++ src/main/java/com/TheWandererRaven/ravencoffee/util/registries/BlocksRegistry.java
@@ -4,6 +4,12 @@ import com.TheWandererRaven.ravencoffee.RavenCoffee;
 import com.TheWandererRaven.ravencoffee.blocks.CoffeeBeansRoastedBlock;
 import com.TheWandererRaven.ravencoffee.blocks.CoffeeBeansRoastedMagmaBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BushBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,5 +23,9 @@ public class BlocksRegistry {
     public static final RegistryObject<Block> COFFEE_BEANS_ROASTED_MAGMA_BLOCK = BLOCKS.register(
             "coffee_beans_roasted_magma_block",
             CoffeeBeansRoastedMagmaBlock::new
+    );
+    public static final RegistryObject<Block> COFFEE_BEANS_BUSH_BLOCK = BLOCKS.register(
+            "coffee_bush_block",
+            () -> new CoffeeBeansBushBlock(AbstractBlock.Properties.create(Material.CACTUS).sound(SoundType.PLANT).notSolid())
     );
 }
