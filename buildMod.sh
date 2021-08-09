10954b8e1545d782616863f9e628ec203515ccc4
TheWandererRaven <gabrielcorderor@gmail.com>
2021-08-09 13:26:27 -0500

    Updated buildmod shell script

--- buildMod.sh
+++ buildMod.sh
@@ -1,3 +1,3 @@
 cd build/libs;
 mv modid-1.0.jar ravencoffee-1.0.jar;
-cp ravencoffee-1.0.jar C:/Users/GabrielCorderoRamire/AppData/Roaming/.minecraft/mods
+cp ravencoffee-1.0.jar C:/Users/gcr61823/AppData/Roaming/.minecraft/mods
