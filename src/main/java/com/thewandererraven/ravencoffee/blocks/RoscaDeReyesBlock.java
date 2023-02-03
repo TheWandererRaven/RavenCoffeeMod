596b025feecff39e59fe99061b007f0ac34a58b9
TheWandererRaven <gabrielcorderor@gmail.com>
2023-02-03 02:10:33 -0600

    Added random drop for Rosca de Reyes, recipe and lang data

--- src/main/java/com/thewandererraven/ravencoffee/blocks/RoscaDeReyesBlock.java
+++ src/main/java/com/thewandererraven/ravencoffee/blocks/RoscaDeReyesBlock.java
@@ -1,22 +1,17 @@
 package com.thewandererraven.ravencoffee.blocks;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.stats.Stats;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CandleBlock;
-import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -66,7 +61,18 @@ public class RoscaDeReyesBlock extends Block {
             }
         }
 
-        return eat(level, blockPos, blockState, player);
+        InteractionResult eatRes = eat(level, blockPos, blockState, player);
+        if(eatRes == InteractionResult.SUCCESS) {
+            int rand = level.getRandom().nextInt(60000);
+            if (rand < 1) {
+                ItemEntity droppedItem = new ItemEntity(
+                        level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), new ItemStack(Items.TOTEM_OF_UNDYING)
+                );
+                droppedItem.setDefaultPickUpDelay();
+                level.addFreshEntity(droppedItem);
+            }
+        }
+        return eatRes;
     }
 
     protected static InteractionResult eat(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, Player player) {
