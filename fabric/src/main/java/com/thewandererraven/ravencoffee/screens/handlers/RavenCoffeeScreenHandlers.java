d2ddd774d4b2344bc1ce3ce1af419cd32ec1489c
TheWandererRaven <gabrielcorderor@gmail.com>
2024-07-20 00:57:26 -0600

    Registered screen gandlers (fabric)

--- fabric/src/main/java/com/thewandererraven/ravencoffee/screens/handlers/RavenCoffeeScreenHandlers.java
+++ fabric/src/main/java/com/thewandererraven/ravencoffee/screens/handlers/RavenCoffeeScreenHandlers.java
@@ -1,15 +1,25 @@
 package com.thewandererraven.ravencoffee.screens.handlers;
 
+import com.thewandererraven.ravencoffee.Constants;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class RavenCoffeeScreenHandlers {
-    public static ScreenHandlerType<SackScreenHandler> SACK_SCREEN_HANDLER;
-    public static ScreenHandlerType<CoffeeGrinderScreenHandler> COFFEE_GRINDER_SCREEN_HANDLER;
-    public static ScreenHandlerType<CoffeeMachineScreenHandler> COFFEE_MACHINE_SCREEN_HANDLER;
+    public static ScreenHandlerType<SackScreenHandler> SACK_SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER,
+            new Identifier(Constants.MOD_ID, "sack"),
+            new ScreenHandlerType<>(SackScreenHandler::new)
+    );
+    public static ScreenHandlerType<CoffeeGrinderScreenHandler> COFFEE_GRINDER_SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER,
+            new Identifier(Constants.MOD_ID, "coffee_grinder"),
+            new ScreenHandlerType<>(CoffeeGrinderScreenHandler::new)
+    );
+    public static ScreenHandlerType<CoffeeMachineScreenHandler> COFFEE_MACHINE_SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER,
+            new Identifier(Constants.MOD_ID, "coffee_machine"),
+            new ScreenHandlerType<>(CoffeeMachineScreenHandler::new)
+    );
 
     public static void registerAllScreenHandlers() {
-        SACK_SCREEN_HANDLER = new ScreenHandlerType<>(SackScreenHandler::new);
-        COFFEE_GRINDER_SCREEN_HANDLER = new ScreenHandlerType<>(CoffeeGrinderScreenHandler::new);
-        COFFEE_MACHINE_SCREEN_HANDLER = new ScreenHandlerType<>(CoffeeMachineScreenHandler::new);
+        Constants.LOGGER.info("Registering Screen Handlers for " + Constants.MOD_ID);
     }
 }
