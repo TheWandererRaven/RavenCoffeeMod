82dcc15c6a371ef11c887406d90c4a9b0b5b8db6
TheWandererRaven <gabrielcorderor@gmail.com>
2024-08-18 16:28:34 -0600

    [Forge] Updated World Gen

--- forge/src/main/java/com/thewandererraven/ravencoffee/world/features/RavenCoffeeConfiguredFeatures.java
+++ forge/src/main/java/com/thewandererraven/ravencoffee/world/features/RavenCoffeeConfiguredFeatures.java
@@ -2,26 +2,42 @@ package com.thewandererraven.ravencoffee.world.features;
 
 import com.google.common.collect.ImmutableSet;
 import com.thewandererraven.ravencoffee.Constants;
+import com.thewandererraven.ravencoffee.blocks.CoffeeTreeTrunkBlock;
 import com.thewandererraven.ravencoffee.world.features.configs.DualBlockPileFeatureConfig;
 import com.thewandererraven.ravencoffee.blocks.RavenCoffeeBlocks;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.SimpleStateProvider;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
 
 public class RavenCoffeeConfiguredFeatures {
-    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES = DeferredRegister.create(
-            Registry.CONFIGURED_FEATURE_REGISTRY, Constants.MOD_ID
-    );
 
-    public static final RegistryObject<ConfiguredFeature<?, ?>> COFFEE_TREE = CONFIGURED_FEATURES.register(
-            "coffee_tree_configured_feature",
-            () -> new ConfiguredFeature<>(
-                    RavenCoffeeFeatures.COFFEE_TREE.get(),
-                    FeatureConfigs.COFFEE_TREE_PATCH_DEFAULT_CONFIG)
-    );
+    public static final ResourceKey<ConfiguredFeature<?, ?>> COFFEE_TREE_PATCH_DEFAULT_KEY = registerKey("coffee_tree_patch_default");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> COFFEE_TREE_PATCH_JUNGLE_KEY = registerKey("coffee_tree_patch_jungle");
+
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        register(context, COFFEE_TREE_PATCH_DEFAULT_KEY,
+                RavenCoffeeFeatures.COFFEE_TREE.get(),
+                FeatureConfigs.COFFEE_TREE_PATCH_DEFAULT_CONFIG
+        );
+        register(context, COFFEE_TREE_PATCH_JUNGLE_KEY,
+                RavenCoffeeFeatures.COFFEE_TREE.get(),
+                FeatureConfigs.COFFEE_TREE_PATCH_JUNGLE_CONFIG
+        );
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(Constants.MOD_ID, name));
+    }
+
+    public static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC config) {
+        context.register(key, new ConfiguredFeature(feature, config));
+    }
 
     public static class FeatureConfigs {
         public static final DualBlockPileFeatureConfig COFFEE_TREE_PATCH_DEFAULT_CONFIG = (
@@ -48,7 +64,7 @@ public class RavenCoffeeConfiguredFeatures {
                 .build();
     }
     public static class States {
-        protected static final BlockState COFFEE_TREE_TRUNK = RavenCoffeeBlocks.COFFEE_TREE_TRUNK_BLOCK.get().defaultBlockState();
+        protected static final BlockState COFFEE_TREE_TRUNK = RavenCoffeeBlocks.COFFEE_TREE_TRUNK_BLOCK.get().defaultBlockState().setValue(CoffeeTreeTrunkBlock.HAS_LEAVES, true);
         protected static final BlockState COFFEE_TREE_LEAVES = RavenCoffeeBlocks.COFFEE_TREE_LEAVES_BLOCK.get().defaultBlockState();
         protected static final BlockState GRASS_BLOCK = net.minecraft.world.level.block.Blocks.GRASS_BLOCK.defaultBlockState();
     }
